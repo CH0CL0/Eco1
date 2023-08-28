5a35cf10fd1120773c5659a6e8be39149da923d5
MateoGuevara09 <97854957+MateoGuevara09@users.noreply.github.com>
2023-08-28 10:37:55 -0300

    mas preguntas

--- mi-proyecto-react/src/App.js
+++ mi-proyecto-react/src/App.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
-
+//AGREGAR UN BOTON QUE DIGA MAS INFROMACION DESPUES DE LA RESPUESTA DE CADA PREGUNTA Y QUE LLEVE A OTRA PAGINA CON LA EXPLICACION(USANDO REACT NO SE QUE)
 const preguntas = [
   {
-    pregunta: '¿Cuál es la capital de Francia?',
-    opciones: ['Londres', 'París', 'Madrid', 'Roma'],
-    respuestaCorrecta: 'París',
+    pregunta: '¿Cual de estos compuestos no pertenece a la lista?',
+    opciones: ['CO2', 'CH4', 'N20', 'H20'],
+    respuestaCorrecta: 'H20',
+    explicacion: 'ya que no es un gas de efecto invernadero',
+    porcentaje: '60'
+  },
+  //{
+    //pregunta: 'Complete la siguiente serie de numeros: 2020,2023,2027,2032, * , 2044,2051',
+    //respuestaCorrecta: 2038,
+    //explicacion:'Si no se para el cambio climatico para 2038 el mundo va a explotar',
+    //porcentaje: '70'
+  //},
+  {
+    pregunta: 'Cual de las siguientes palabras no encaja en la siguiente seire:',
+    opciones: ['Desolacion', 'Insolar', 'Consolador', 'Invernadero'],
+    respuestaCorrecta: 'Invernadero',
+    explicacion: 'Invernadero es la unica palabra que no tiene sol dentro de la palabra (poner mas info del sol)',
+    porcentaje: '50'
+  },
+  //{
+    //pregunta: 'Cual es la palabra mas larga de esta pregunta que no contiene ninguna de las letras de la palabra "Clima"',
+    //respuestaCorrecta: 'que',
+    //explicacion: '"es", "no" no contienen ninguna letra pero "que" es la mas larga.',
+    //porcentaje: '40'
+  //},
+  {
+    pregunta: 'Cualc del clos siguilentes vivens sen lel mhar',
+    opciones: ['abra', 'eduza', 'eon', 'aca'],
+    respuestaCorrecta: 'eduza',
+    explicacion: 'La pregunta tiene letra de mas, y las respuestas letras de menos. M eduza es el unico animal que vive en el mar, ademas estas se ven grandemente afectadas por el cambio climatico.',
+    porcentaje: 15
   },
   {
-    pregunta: '¿Cuántos continentes hay en el mundo?',
-    opciones: ['4', '5', '6', '7'],
-    respuestaCorrecta: '7',
+    pregunta: '¿Cual de las siguientes palabras tiene mas letras?',
+    opciones: ['Mitigacion', 'Millonario'],
+    respuestaCorrecta: 'Mitigacion',
+    explicacion: 'En mitigacion, repite la letra i tres veces, por lo que tiene 8, ademas , la mitigacion es lo que todas las personas tenemos que buscar para asi contrarestar al cambio climatico',
+    porcentaje: 30
+  },
+  {
+    pregunta: 'Cual de los siguientes nombres no pertenece a la lista',
+    opciones: ['Benjanim Baker ', 'Catherine Clark', 'Daniel Davis', 'Greta Thunberg'],
+    respuestaCorrecta: 'Greta Thunberg',
+    explicacion: 'Greta Thumberg. Es el unico nombre que no empieza por la misma inicial tanto su nombre como su apellido, ademas , es una gran activista en contra del cambio climatico.',
+    porcentaje: 80
   },
   // Agrega más preguntas aquí
 ];
-
+//obtener pregunta por porcentaje 
 class App extends Component {
   constructor(props) {
     super(props);
@@ -45,7 +82,7 @@ class App extends Component {
       this.nextQuestion();
     }
   };
-
+//aumenta el porcentaje y busca uno menor
   nextQuestion = () => {
     this.setState({
       preguntaActual: this.getRandomQuestion(),
@@ -54,14 +91,14 @@ class App extends Component {
       showModal: false,
     });
   };
-
+//agregar un boton que diga mas informacion despues de responder , ademas de tener la justificacion.
   render() {
     const { preguntaActual, respuestaSeleccionada, showModal } = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Pregunta Random</h1>
+          <h1>ECO 1%</h1>
           <p>{preguntaActual.pregunta}</p>
           <div className="options">
             {preguntaActual.opciones.map((opcion, index) => (
@@ -87,3 +124,6 @@ class App extends Component {
 }
 
 export default App;
+//mas preguntas
+//Pregunta 3: ¿Cuál es el fenómeno climático extremo que se caracteriza por la elevación rápida del nivel del mar debido al derretimiento acelerado de los glaciares y capas de hielo?
+//Respuesta: Este fenómeno se llama la "subida del nivel del mar" o el "incremento del nivel del mar".
