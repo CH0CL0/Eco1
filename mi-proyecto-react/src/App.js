070b9cceb73247ed0034147a6f65b77bc512a45f
ALESSANDRO MATIAS ARENA <46917132@comunidadort.edu.ar>
2023-09-04 10:40:09 -0300

    lol

--- mi-proyecto-react/src/App.js
+++ mi-proyecto-react/src/App.js
@@ -1,57 +1,15 @@
+//React
 import React, { Component } from 'react';
-import './App.css';
-//AGREGAR UN BOTON QUE DIGA MAS INFROMACION DESPUES DE LA RESPUESTA DE CADA PREGUNTA Y QUE LLEVE A OTRA PAGINA CON LA EXPLICACION(USANDO REACT NO SE QUE)
-const preguntas = [
 
-  {
-    pregunta: 'Cualc del clos siguilentes vivens sen lel mhar',
-    opciones: ['abra', 'eduza', 'eon', 'aca'],
-    respuestaCorrecta: 'eduza',
-    explicacion: 'La pregunta tiene letra de mas, y las respuestas letras de menos. M eduza es el unico animal que vive en el mar, ademas estas se ven grandemente afectadas por el cambio climatico.',
-    porcentaje: 20
-  },
-  {
-    pregunta: '¿Cual de las siguientes palabras tiene mas letras?',
-    opciones: ['Mitigacion', 'Millonario'],
-    respuestaCorrecta: 'Mitigacion',
-    explicacion: 'En mitigacion, repite la letra i tres veces, por lo que tiene 8, ademas , la mitigacion es lo que todas las personas tenemos que buscar para asi contrarestar al cambio climatico',
-    porcentaje: 30
-  },
-    {
-    pregunta: 'Cual es la palabra mas larga de esta pregunta que no contiene ninguna de las letras de la palabra "Clima"',
-    respuestaCorrecta: 'que',
-    explicacion: '"es", "no" no contienen ninguna letra pero "que" es la mas larga.',
-    porcentaje: 40
-  },
-  {
-    pregunta: 'Cual de las siguientes palabras no encaja en la siguiente seire:',
-    opciones: ['Desolacion', 'Insolar', 'Consolador', 'Invernadero'],
-    respuestaCorrecta: 'Invernadero',
-    explicacion: 'Invernadero es la unica palabra que no tiene sol dentro de la palabra (poner mas info del sol)',
-    porcentaje: 50
-  },
-  {
-    pregunta: '¿Cual de estos compuestos no pertenece a la lista?',
-    opciones: ['CO2', 'CH4', 'N20', 'H20'],
-    respuestaCorrecta: 'H20',
-    explicacion: 'ya que no es un gas de efecto invernadero',
-    porcentaje: 60
-  },
-  {
-    pregunta: 'Complete la siguiente serie de numeros: 2020,2023,2027,2032, * , 2044,2051',
-    respuestaCorrecta: '2038',
-    explicacion:'Si no se para el cambio climatico para 2038 el mundo va a explotar',
-    porcentaje: 70
-  },
-  {
-    pregunta: 'Cual de los siguientes nombres no pertenece a la lista',
-    opciones: ['Benjanim Baker ', 'Catherine Clark', 'Daniel Davis', 'Greta Thunberg'],
-    respuestaCorrecta: 'Greta Thunberg',
-    explicacion: 'Greta Thumberg. Es el unico nombre que no empieza por la misma inicial tanto su nombre como su apellido, ademas , es una gran activista en contra del cambio climatico.',
-    porcentaje: 80
-  },
-  // Agrega más preguntas aquí
-];
+//Bootstrap
+
+//css
+import './App.css';
+
+//Other
+import  preguntas from './preguntas/preguntas';
+
+//AGREGAR UN BOTON QUE DIGA MAS INFROMACION DESPUES DE LA RESPUESTA DE CADA PREGUNTA Y QUE LLEVE A OTRA PAGINA CON LA EXPLICACION(USANDO REACT NO SE QUE)
 //obtener pregunta por porcentaje 
 class App extends Component {
   constructor(props) {
