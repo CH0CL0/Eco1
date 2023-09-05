3c33b4d6bbe1a4fd302c2b70a857c304ea424529
CH0CL0 <arenaalessandro156@gmail.com>
2023-09-05 18:44:43 -0300

    esta re bugeado

--- mi-proyecto-react/src/App.js
+++ mi-proyecto-react/src/App.js
@@ -1,68 +1,55 @@
-//React
 import React, { Component } from 'react';
-
-//Bootstrap
-
-//css
 import './App.css';
+import preguntas from './preguntas/preguntas';
 
-//Other
-import  preguntas from './preguntas/preguntas';
-
-//AGREGAR UN BOTON QUE DIGA MAS INFROMACION DESPUES DE LA RESPUESTA DE CADA PREGUNTA Y QUE LLEVE A OTRA PAGINA CON LA EXPLICACION(USANDO REACT NO SE QUE)
-//obtener pregunta por porcentaje 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       preguntasRestantes: preguntas.slice().sort((a, b) => a.porcentaje - b.porcentaje),
       preguntaActual: null,
-      respuestaUsuario: "", // Almacenar la respuesta escrita por el usuario
+      respuestaUsuario: "",
       respuestaCorrecta: false,
-      opcionesSeleccionadas: [], // Un array para rastrear las opciones seleccionadas
+      opcionesSeleccionadas: [],
       showModal: false,
+      juegoIniciado: false, 
     };
   }
-
   componentDidMount() {
     this.siguientePregunta();
   }
-
   siguientePregunta = () => {
     if (this.state.preguntasRestantes.length > 0) {
       const preguntaActual = this.state.preguntasRestantes.shift();
       this.setState({
         preguntaActual: preguntaActual,
-        respuestaUsuario: "", // Reiniciar la respuesta del usuario
+        respuestaUsuario: "",
         respuestaCorrecta: false,
-        opcionesSeleccionadas: [], // Reiniciar las opciones seleccionadas
+        opcionesSeleccionadas: [],
         showModal: false,
       });
     } else {
-      // Si no hay más preguntas, puedes manejarlo de acuerdo a tus necesidades
       console.log("No hay más preguntas disponibles.");
     }
   };
-
   checkAnswer = () => {
     const { preguntaActual, respuestaUsuario, opcionesSeleccionadas } = this.state;
     const { respuestaCorrecta, opciones } = preguntaActual;
   
-    // Verificar si la pregunta tiene opciones
+    let juegoReiniciado = false; // Variable para verificar si el juego se ha reiniciado
+  
     if (opciones && opciones.length > 0) {
-      // Verificar si al menos una opción ha sido seleccionada
-      const alMenosUnaOpcionSeleccionada = opcionesSeleccionadas.some((seleccionada) => seleccionada);
+      const alMenosUnaOpcionSeleccionada = opcionesSeleccionadas.some(
+        (seleccionada) => seleccionada
+      );
   
       if (alMenosUnaOpcionSeleccionada) {
-        // Comparar las opciones seleccionadas por el usuario con las opciones correctas
-        const opcionesCorrectas = opciones.map((opcion, index) =>
-          opcion.trim().toLowerCase() === respuestaCorrecta.trim().toLowerCase()
+        const opcionesCorrectas = opciones.map(
+          (opcion) => opcion.trim().toLowerCase() === respuestaCorrecta.trim().toLowerCase()
         );
-  
         const respuestasCorrectas = opcionesSeleccionadas.every(
           (seleccionada, index) => seleccionada === opcionesCorrectas[index]
         );
-  
         this.setState({
           respuestaCorrecta: respuestasCorrectas,
           showModal: true,
@@ -70,65 +57,108 @@ class App extends Component {
   
         if (respuestasCorrectas) {
           this.siguientePregunta();
+        } else {
+          // Si la respuesta es incorrecta, marcamos el juego como reiniciado
+          juegoReiniciado = true;
         }
       } else {
-        // Si ninguna opción ha sido seleccionada, marcar como respuesta incorrecta
         this.setState({
           respuestaCorrecta: false,
           showModal: true,
         });
       }
     } else {
-      // Para respuestas de texto libre
-      const respuestaCorrectaSeleccionada = respuestaUsuario.trim().toLowerCase() === respuestaCorrecta.trim().toLowerCase();
-    
+      const respuestaCorrectaSeleccionada =
+        respuestaUsuario.trim().toLowerCase() === respuestaCorrecta.trim().toLowerCase();
       this.setState({
         respuestaCorrecta: respuestaCorrectaSeleccionada,
         showModal: true,
       });
-    
+  
       if (respuestaCorrectaSeleccionada) {
         this.siguientePregunta();
+      } else {
+        // Si la respuesta es incorrecta, marcamos el juego como reiniciado
+        juegoReiniciado = true;
       }
     }
+  
+    // Verificamos si el juego se ha reiniciado y, en ese caso, volvemos a la pantalla de inicio
+    if (juegoReiniciado) {
+      this.setState({
+        juegoIniciado: false,
+      });
+    }
   };
+
+  siguientePregunta = () => {
+  if (this.state.preguntasRestantes.length > 0) {
+    const preguntaActual = this.state.preguntasRestantes.shift();
+    this.setState({
+      preguntaActual: preguntaActual,
+      respuestaUsuario: "",
+      respuestaCorrecta: false,
+      opcionesSeleccionadas: [],
+      showModal: false,
+    });
+  } else {
+    console.log("No hay más preguntas disponibles.");
+    // Verificamos si el juego se ha reiniciado y volvemos a la pantalla de inicio
+    if (this.state.juegoIniciado) {
+      this.setState({
+        juegoIniciado: false,
+      });
+    }
+  }
+};
   
 
   handleInputChange = (event) => {
-    // Actualizar la respuesta del usuario cuando escriba en el campo de entrada
     this.setState({
       respuestaUsuario: event.target.value,
     });
   };
 
   handleOptionClick = (index) => {
-    // Actualizar el estado de opciones seleccionadas cuando el usuario hace clic en un botón de opción
     const nuevasOpcionesSeleccionadas = [...this.state.opcionesSeleccionadas];
     nuevasOpcionesSeleccionadas[index] = !nuevasOpcionesSeleccionadas[index];
-
     this.setState({
       opcionesSeleccionadas: nuevasOpcionesSeleccionadas,
     });
   };
 
-  render() {
-    const { preguntaActual, opcionesSeleccionadas, respuestaCorrecta, showModal } = this.state;
+  iniciarJuego = () => {
+    this.setState({
+      juegoIniciado: true,
+    });
+    this.siguientePregunta(); 
+  };
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>ECO 1%</h1>
-          {preguntaActual && (
+  render() {
+    const { preguntaActual, opcionesSeleccionadas, respuestaCorrecta, showModal, juegoIniciado } = this.state;
+
+    if (!juegoIniciado) {
+      // Si el juego aún no ha comenzado, mostrar la pantalla de inicio
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Eco1</h1>
+            <button className="start-button" onClick={this.iniciarJuego}>Jugar</button>
+          </header>
+        </div>
+      );
+    }
+      return (
+        <div className="App">
+          <header className="App-header">
+            {preguntaActual && (
             <div>
               <p>{preguntaActual.pregunta}</p>
-              {/* Mostrar opciones para las preguntas con opciones */}
               {preguntaActual.opciones && preguntaActual.opciones.length > 0 ? (
-                <div className="options">
-                  {preguntaActual.opciones.map((opcion, index) => (
-                    <button
+                <div className="options">{preguntaActual.opciones.map((opcion, index) => (<button
                       key={index}
                       className={`option ${
-                        opcionesSeleccionadas[index] === true ? "selected" : ""
+                      opcionesSeleccionadas[index] === true ? "selected" : ""
                       }`}
                       onClick={() => this.handleOptionClick(index)}
                     >
@@ -146,14 +176,13 @@ class App extends Component {
                   />
                 </div>
               )}
-              {/* Botón para verificar respuestas */}
               <button class="check-button" onClick={this.checkAnswer}>Verificar Respuestas</button>
             </div>
           )}
           {showModal && (
             <div className="modal">
-              <p>{respuestaCorrecta ? "¡Correcto!" : "¡Incorrecto!"}</p>
-              <button onClick={this.siguientePregunta}>Siguiente Pregunta</button>
+              <p>{respuestaCorrecta ? "Genial! Lograste responder todas las preguntas con éxito" : "¡Incorrecto!"}</p>
+              <button onClick={this.iniciarJuego}>Volver al inicio</button>
             </div>
           )}
         </header>
@@ -163,8 +192,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-//mas preguntas
-//Pregunta 3: ¿Cuál es el fenómeno climático extremo que se caracteriza por la elevación rápida del nivel del mar debido al derretimiento acelerado de los glaciares y capas de hielo?
-//Respuesta: Este fenómeno se llama la "subida del nivel del mar" o el "incremento del nivel del mar".
--- mi-proyecto-react/src/preguntas/preguntas.js
+++ mi-proyecto-react/src/preguntas/preguntas.js
@@ -1,78 +1,71 @@
 const preguntas = [
-        {
-            pregunta: 'Cualc del clos siguilentes vivens sen lel mhar',
-            opciones: ['abra', 'eduza', 'eon', 'aca'],
-            respuestaCorrecta: 'eduza',
-            explicacion: 'La pregunta tiene letra de mas, y las respuestas letras de menos. M eduza es el unico animal que vive en el mar, ademas estas se ven grandemente afectadas por el cambio climatico.',
-            porcentaje: 20
-        },
-      {
-        pregunta: '¿Cual de las siguientes palabras tiene mas letras?',
-        opciones: ['Mitigacion', 'Millonario'],
-        respuestaCorrecta: 'Mitigacion',
-        explicacion: 'En mitigacion, repite la letra i tres veces, por lo que tiene 8, ademas , la mitigacion es lo que todas las personas tenemos que buscar para asi contrarestar al cambio climatico',
-        porcentaje: 30
-      },
-        {
-        pregunta: 'Cual es la palabra mas larga de esta pregunta que no contiene ninguna de las letras de la palabra "Clima"',
-        respuestaCorrecta: 'que',
-        explicacion: '"es", "no" no contienen ninguna letra pero "que" es la mas larga.',
-        porcentaje: 40
-      },
-      {
-        pregunta: 'Cual de las siguientes palabras no encaja en la siguiente seire:',
-        opciones: ['Desolacion', 'Insolar', 'Consolador', 'Invernadero'],
-        respuestaCorrecta: 'Invernadero',
-        explicacion: 'Invernadero es la unica palabra que no tiene sol dentro de la palabra (poner mas info del sol)',
-        porcentaje: 50
-      },
-      {
-        pregunta: '¿Cual de estos compuestos no pertenece a la lista?',
-        opciones: ['CO2', 'CH4', 'N20', 'H20'],
-        respuestaCorrecta: 'H20',
-        explicacion: 'ya que no es un gas de efecto invernadero',
-        porcentaje: 60
-      },
-      {
-        pregunta: 'Complete la siguiente serie de numeros: 2020,2023,2027,2032, * , 2044,2051',
-        respuestaCorrecta: '2038',
-        explicacion:'Si no se para el cambio climatico para 2038 el mundo va a explotar',
-        porcentaje: 70
-      },
-      {
-        pregunta: 'Cual de los siguientes nombres no pertenece a la lista',
-        opciones: ['Benjanim Baker ', 'Catherine Clark', 'Daniel Davis', 'Greta Thunberg'],
-        respuestaCorrecta: 'Greta Thunberg',
-        explicacion: 'Greta Thumberg. Es el unico nombre que no empieza por la misma inicial tanto su nombre como su apellido, ademas , es una gran activista en contra del cambio climatico.',
-        porcentaje: 80
-      },
-      {
-        pregunta: "¿Cuál de las siguientes actividades humanas contribuye al aumento del CO2 en la atmósfera?",
-        opciones: ["Uso de automóviles", "Plantación de árboles", "Reciclaje de papel", "Consumo de frutas y verduras"],
-        respuestaCorrecta: "Uso de automóviles",
-        explicación: "Los automóviles emiten CO2 debido a la combustión de gasolina o diésel, lo que contribuye al cambio climático.",
-        porcentaje: 15
-        },
-        {
-            pregunta: "¿Qué causa el aumento del nivel del mar como resultado del cambio climático?",
-            opciones: ["Derretimiento de glaciares y casquetes polares", "Mayor evaporación del agua del océano", "Actividades volcánicas", "Cambio en la órbita terrestre"],
-            respuestaCorrecta: "Derretimiento de glaciares y casquetes polares",
-            explicación: "El derretimiento de hielo en regiones polares contribuye significativamente al aumento del nivel del mar debido al cambio climático.",
-            porcentaje: 25
-        },
-        {
-            pregunta: "¿Qué es la acidificación del océano y cómo está relacionada con el cambio climático?",
-            opciones: ["Aumento del pH del agua del océano debido al CO2", "Disminución del nivel del mar", "Mayor salinidad del océano", "Mayor temperatura del océano"],
-            respuestaCorrecta: "Aumento del pH del agua del océano debido al CO2",
-            explicación: "El CO2 atmosférico se disuelve en el agua del océano, lo que reduce el pH y afecta a los organismos marinos y los ecosistemas.",
-            porcentaje: 18
-        },
-        {
-            pregunta: "¿Impacto en la biodiversidad por cambio climático?",
-            respuestaCorrecta: "Extinción",
-            explicacion: "El cambio climático provoca alteraciones en los ecosistemas que pueden llevar a la extinción de diversas especies debido a cambios en su hábitat y condiciones de vida.",
-            porcentaje: 15
-        },
-      // Agrega más preguntas aquí
+  {
+    pregunta: "El mayor impacto en la biodiversidad por cambio climático es: ",
+    respuestaCorrecta: "Extinción",
+    explicacion: "El cambio climático provoca alteraciones en los ecosistemas que pueden llevar a la extinción de diversas especies debido a cambios en su hábitat y condiciones de vida.",
+    porcentaje: 10
+  },
+  {
+    pregunta: 'Cualc del clos siguilentes vivens sen lel mhar',
+    opciones: ['abra', 'eduza', 'eon', 'aca'],
+    respuestaCorrecta: 'eduza',
+    explicacion: 'La pregunta tiene letra de mas, y las respuestas letras de menos. M eduza es el unico animal que vive en el mar, ademas estas se ven grandemente afectadas por el cambio climatico.',
+    porcentaje: 20
+  },
+  {
+    pregunta: '¿Cual de las siguientes palabras tiene mas letras?',
+    opciones: ['Mitigacion', 'Millonario'],
+    respuestaCorrecta: 'Mitigacion',
+    explicacion: 'En mitigacion, repite la letra i tres veces, por lo que tiene 8, ademas , la mitigacion es lo que todas las personas tenemos que buscar para asi contrarestar al cambio climatico',
+    porcentaje: 30
+  },
+  {
+    pregunta: '¿Cual de estos compuestos no pertenece a la lista?',
+    opciones: ['CO2', 'CH4', 'N20', 'H20'],
+    respuestaCorrecta: 'H20',
+    explicacion: 'ya que no es un gas de efecto invernadero',
+    porcentaje: 40
+  },
+  {
+    pregunta: 'Cual de las siguientes palabras no encaja en la siguiente seire:',
+    opciones: ['Desolacion', 'Insolar', 'Consolador', 'Invernadero'],
+    respuestaCorrecta: 'Invernadero',
+    explicacion: 'Invernadero es la unica palabra que no tiene sol dentro de la palabra (poner mas info del sol)',
+    porcentaje: 50
+  },
+  {
+    pregunta: "¿Qué es la acidificación del océano y cómo está relacionada con el cambio climático?",
+    opciones: ["Aumento del pH del agua del océano debido al CO2", "Disminución del nivel del mar", "Mayor salinidad del océano", "Mayor temperatura del océano"],
+    respuestaCorrecta: "Aumento del pH del agua del océano debido al CO2",
+    explicación: "El CO2 atmosférico se disuelve en el agua del océano, lo que reduce el pH y afecta a los organismos marinos y los ecosistemas.",
+    porcentaje: 60
+  },
+  {
+    pregunta: 'Complete la siguiente serie de numeros: 2020,2023,2027,2032, * , 2044,2051',
+    respuestaCorrecta: '2038',
+    explicacion: 'Si no se para el cambio climatico para 2038 el mundo va a explotar',
+    porcentaje: 70
+  },
+  {
+    pregunta: 'Cual de los siguientes nombres no pertenece a la lista',
+    opciones: ['Benjanim Baker ', 'Catherine Clark', 'Daniel Davis', 'Greta Thunberg'],
+    respuestaCorrecta: 'Greta Thunberg',
+    explicacion: 'Greta Thumberg. Es el unico nombre que no empieza por la misma inicial tanto su nombre como su apellido, ademas , es una gran activista en contra del cambio climatico.',
+    porcentaje: 80
+  },
+  {
+    pregunta: "¿Cuál de las siguientes actividades humanas contribuye al aumento del CO2 en la atmósfera?",
+    opciones: ["Uso de automóviles", "Plantación de árboles", "Reciclaje de papel", "Consumo de frutas y verduras"],
+    respuestaCorrecta: "Uso de automóviles",
+    explicación: "Los automóviles emiten CO2 debido a la combustión de gasolina o diésel, lo que contribuye al cambio climático.",
+    porcentaje: 80
+  },
+  {
+    pregunta: "¿Qué causa el aumento del nivel del mar como resultado del cambio climático?",
+    opciones: ["Derretimiento de glaciares y casquetes polares", "Mayor evaporación del agua del océano", "Actividades volcánicas", "Cambio en la órbita terrestre"],
+    respuestaCorrecta: "Derretimiento de glaciares y casquetes polares",
+    explicación: "El derretimiento de hielo en regiones polares contribuye significativamente al aumento del nivel del mar debido al cambio climático.",
+    porcentaje: 90
+  },
 ]
 export default preguntas;
