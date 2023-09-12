1eb5eee53b74baff6672a2d51c5a48760563dd06
CH0CL0 <arenaalessandro156@gmail.com>
2023-09-12 20:26:26 -0300

    asdasda

--- mi-proyecto-react/src/components/Juego.jsx
+++ mi-proyecto-react/src/components/Juego.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom"; // Import useHistory
+import { useHistory } from "react-router-dom";
 import '../App.css';
 import preguntas from '../preguntas/preguntas';
 
